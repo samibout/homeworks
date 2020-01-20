fcd4d50eae7cb5a4f157b4111c81e52eac3294bd
Sammien <sammi.sjodahl@passagen.se>
2020-01-20 13:45:15 +0200

    ex10done

--- part1/printing/exercise_10/Program.cs
+++ part1/printing/exercise_10/Program.cs
@@ -7,7 +7,13 @@ namespace exercise_10
         public static void Main(string[] args)
         {
             // Write your code here:
+            Console.WriteLine("This is a story telling machine. \nPlease give your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("And your profession");
+            string profs = Console.ReadLine();
 
+            Console.WriteLine("There was a cat named "+name+" and a big cat that was. \nIt has a dream of being a famous "+profs
+            +" and later on that it was");
         }
     }
 }
