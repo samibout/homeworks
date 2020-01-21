87d8e3274d1c245737bcb9184664e4e06b65cc87
Sammien <sammi.sjodahl@passagen.se>
2020-01-21 08:01:15 +0200

    ex19done

--- part1/calculations/exercise_19/Program.cs
+++ part1/calculations/exercise_19/Program.cs
@@ -8,7 +8,15 @@ namespace exercise_19
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      int first = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Give the second number!");
+      int second = Convert.ToInt32(Console.ReadLine());
 
+      int sum = first + second;          
+
+      Console.WriteLine(first + " + " + second + " = " + sum);
     }
   }
 }
