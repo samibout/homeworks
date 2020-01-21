398a6a986813779ec5744f23da432821725ad0df
Sammien <sammi.sjodahl@passagen.se>
2020-01-21 17:48:40 +0200

    ex30done

--- part1/conditionals/exercise_30/Program.cs
+++ part1/conditionals/exercise_30/Program.cs
@@ -8,6 +8,48 @@ namespace exercise_30
     {
       // Write your code here:
 
-    }
+      int grade=0;
+      Console.WriteLine("Give your percent [0 - 100]:");
+      int percent = Convert.ToInt32(Console.ReadLine());
+
+      if(percent<0)
+      {  
+        Console.WriteLine("Impossible");
+        
+      }
+      else if(percent<50)
+      {  
+        Console.WriteLine("Fail");
+      }
+      else if(percent>100)
+      {  
+        Console.WriteLine("Outstanding!");
+      }
+      else if(percent<60)
+      {  
+        grade = 1;
+      }
+      else if(percent<70)
+      {  
+        grade = 2;
+      }
+      else if(percent<80)
+      {  
+        grade = 3;
+      }
+      else if(percent<90)
+      {  
+        grade = 4;
+      }
+      else if(percent<=100)
+      {  
+        grade = 5;
+      }  
+
+      if (grade > 0)
+      {
+        Console.WriteLine("Grade: " + grade);
+      }    
+    }  
   }
 }
