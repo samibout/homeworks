dfa8cd4d29d5a0f151d344a20333c406592a00c2
Sammien <sammi.sjodahl@passagen.se>
2020-01-21 08:27:52 +0200

    ex23done

--- part1/calculations/exercise_23/Program.cs
+++ part1/calculations/exercise_23/Program.cs
@@ -8,7 +8,22 @@ namespace exercise_23
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      int first = Convert.ToInt32(Console.ReadLine());
+      double firstD = Convert.ToDouble(first);
+      
+      Console.WriteLine("Give the second number!");
+      int second = Convert.ToInt32(Console.ReadLine());
 
+      int sum = first + second;
+      int sub = first - second;
+      int mul = first * second;
+      double div = firstD / second;
+      
+      Console.WriteLine(first + " + " + second + " = " + sum);
+      Console.WriteLine(first + " - " + second + " = " + sub);
+      Console.WriteLine(first + " * " + second + " = " + mul);
+      Console.WriteLine(first + " / " + second + " = " + div);
     }
   }
 }
