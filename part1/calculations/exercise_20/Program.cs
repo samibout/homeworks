3058c35eea605015bf20faf5820908356d431292
Sammien <sammi.sjodahl@passagen.se>
2020-01-21 08:04:33 +0200

    ex20done

--- part1/calculations/exercise_20/Program.cs
+++ part1/calculations/exercise_20/Program.cs
@@ -8,7 +8,15 @@ namespace exercise_20
     {
 
       // Write your code here:
+      Console.WriteLine("Give the first number!");
+      int first = Convert.ToInt32(Console.ReadLine());
+      
+      Console.WriteLine("Give the second number!");
+      int second = Convert.ToInt32(Console.ReadLine());
 
+      int multi = first * second;          
+
+      Console.WriteLine(first + " * " + second + " = " + multi);
     }
   }
 }
