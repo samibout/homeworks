eb8e137b61f302374566d5dcd6a93d782d754bd2
Sammien <sammi.sjodahl@passagen.se>
2020-01-23 18:49:31 +0200

    ex38done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -7,7 +7,24 @@ namespace exercise_38
     public static void Main(string[] args)
     {
       // Write your code here:
+      int answer = 1;
+      int numbers = 0;
 
+      while(answer != 0)
+      {
+        Console.WriteLine("Give a number:");
+        answer = Convert.ToInt32(Console.ReadLine());
+
+        if(answer == 0)
+        {
+          Console.WriteLine("Total amount of negative numbers: " + numbers);
+        }
+        else if(answer<0)
+        {
+          numbers++;
+        }  
+        
+      }
     }
   }
 }
