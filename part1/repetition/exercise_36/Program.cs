e8f01039eb2871b12fec298e3b6eb4e0e96525b9
Sammien <sammi.sjodahl@passagen.se>
2020-01-23 18:34:00 +0200

    ex36done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -7,7 +7,22 @@ namespace exercise_36
     public static void Main(string[] args)
     {
       // Write your code here:
+      int answer = 1;
 
+      while(answer != 0)
+      {
+        Console.WriteLine("Give a number:");
+        answer = Convert.ToInt32(Console.ReadLine());
+
+        if(answer < 0)
+        {
+          Console.WriteLine("That is negative");
+        }
+        else if(answer>0)
+        {
+          Console.WriteLine(answer*answer);
+        }
+      }  
     }
   }
 }
