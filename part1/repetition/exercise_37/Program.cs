09a8b0536733a2f7e1a06490e5b2f72e5994ca39
Sammien <sammi.sjodahl@passagen.se>
2020-01-23 18:41:23 +0200

    ex37done

--- part1/repetition/exercise_37/Program.cs
+++ part1/repetition/exercise_37/Program.cs
@@ -6,9 +6,23 @@ namespace exercise_37
   {
     public static void Main(string[] args)
     {
-      int sum = 0;
+      //int sum = 0;
       // Write your code here:
+      int answer = 1;
+      int numbers = 0;
 
+      while(answer != 0)
+      {
+        Console.WriteLine("Give a number:");
+        answer = Convert.ToInt32(Console.ReadLine());
+
+        if(answer == 0)
+        {
+          Console.WriteLine("Total amount of numbers: " + numbers);
+        }
+        numbers++;
+        
+      }
     }
   }
 }
