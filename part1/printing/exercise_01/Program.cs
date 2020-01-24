650da41cbd085d2f9a6023b74fa126fcae95234a
Sammien <sammi.sjodahl@passagen.se>
2020-01-24 10:38:05 +0200

    01done

--- part1/printing/exercise_01/Program.cs
+++ part1/printing/exercise_01/Program.cs
@@ -7,9 +7,5 @@ namespace exercise_01
         public static void Main(string[] args) =>
             // Add your code here:
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
-=======
-        }
->>>>>>> 69116e43964f66c5646ed25df07fab71ad6080eb
-    }
-}
+    } 
+}           
