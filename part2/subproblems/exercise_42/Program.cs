6abd1d182d11d03e74b02db38a9361a2bef55fea
Sammien <sammi.sjodahl@passagen.se>
2020-01-26 19:38:14 +0200

    ex42done

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -8,7 +8,14 @@ namespace exercise_42
     {
 
       // Write your code here:
-    
+      int number1 = Convert.ToInt32(Console.ReadLine());
+
+      int number2 = Convert.ToInt32(Console.ReadLine());
+
+      int sum = number1 + number2;
+      double square = Math.Sqrt(sum);
+
+      Console.WriteLine(square);
     }
   }
 }
