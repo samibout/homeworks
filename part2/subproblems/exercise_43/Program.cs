2bc9430f74e5a3654ca7e51460f89f3160621ef9
Sammien <sammi.sjodahl@passagen.se>
2020-01-26 19:54:23 +0200

    ex43done

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_43
     public static void Main(string[] args)
     {
       // Write your code here:
-      
+      int number;
+      number = Convert.ToInt32(Console.ReadLine());
+
+      if(number<0)
+      {
+        Console.WriteLine(number * -1);
+      }  
+      else
+      {
+        Console.WriteLine(number);
+      } 
     }
   }
 }
