9bae5ff61d5adfdf795607c402e2b9910b67fff9
Sammien <sammi.sjodahl@passagen.se>
2020-01-27 09:49:25 +0200

    ex44done

--- part2/subproblems/exercise_44/Program.cs
+++ part2/subproblems/exercise_44/Program.cs
@@ -8,7 +8,21 @@ namespace exercise_44
     {
 
       // Write your code here:
+      int number1 = Convert.ToInt32(Console.ReadLine());
+      int number2 = Convert.ToInt32(Console.ReadLine());
 
+      if(number1<number2)
+      {
+        Console.WriteLine(number1 + " is less than " + number2 + ".");
+      }
+      else if(number1>number2)
+      {
+        Console.WriteLine(number1 + " is greater than " + number2 + ".");
+      }
+      else
+      {
+        Console.WriteLine(number2 + " is equal to " + number1 + ".");
+      }
     }
   }
 }
