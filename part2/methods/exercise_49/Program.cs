67a373a3325984f06556d71fe6ffc1cd8bd0a407
Sammien <sammi.sjodahl@passagen.se>
2020-01-27 20:46:01 +0200

    ex49done

--- part2/methods/exercise_49/Program.cs
+++ part2/methods/exercise_49/Program.cs
@@ -6,13 +6,21 @@ namespace exercise_49
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("How many times?");
+      int number = Convert.ToInt32(Console.ReadLine());
+
       // Call your method here:
- 
+      for (int i=0;i<number;i++)
+
+      {
+        PrintPhrase();
+      }  
     }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase()
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");  
     }
   }
 }
