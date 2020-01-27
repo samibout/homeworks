737eba16edb0c7c093d9a87a2d7a9ac16583f569
Sammien <sammi.sjodahl@passagen.se>
2020-01-27 11:05:20 +0200

    ex48done

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -8,7 +8,42 @@ namespace exercise_48
     {
 
       // Write your code here:
- 
+      int number=0;
+      int amount=0;
+      int sum=0;
+      int even=0;
+      int odd=0;
+      double average;
+
+      Console.WriteLine("Give numbers:");
+      
+      while (number!=-1)
+      {
+        number = Convert.ToInt32(Console.ReadLine());
+        
+        if(number!=-1)
+        {
+          amount++;
+          sum = sum + number;
+          if(number%2==0)
+          {
+            even++;
+          }
+          else
+          {
+            odd++;
+          }
+        }
+      }
+
+      average = Convert.ToDouble(sum)/amount;
+      Console.WriteLine("Thx! Bye!"); 
+      Console.WriteLine("Sum: " + sum);
+      Console.WriteLine("Numbers: " + amount);
+      Console.WriteLine("Average: " + average);
+      Console.WriteLine("Even: " + even);
+      Console.WriteLine("Odd: " + odd);
+
     }
   }
 }
