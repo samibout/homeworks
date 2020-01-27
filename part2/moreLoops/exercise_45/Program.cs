c93c7db266b323788d7c31d66784833aa0bf5d88
Sammien <sammi.sjodahl@passagen.se>
2020-01-27 10:13:18 +0200

    ex45done

--- part2/moreLoops/exercise_45/Program.cs
+++ part2/moreLoops/exercise_45/Program.cs
@@ -8,7 +8,13 @@ namespace exercise_45
     {
 
       // Write your code here:
+      int number;
+      number = Convert.ToInt32(Console.ReadLine());
 
+      for(int i=0;i<=number;i++)
+      {
+        Console.WriteLine(i);
+      }
     }
   }
 }
