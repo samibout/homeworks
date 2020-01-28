1233bbe8720b6d26ffda6642743a4c969b9d973d
Sammien <sammi.sjodahl@passagen.se>
2020-01-28 09:35:13 +0200

    ex52done

--- part2/methods/exercise_52/Program.cs
+++ part2/methods/exercise_52/Program.cs
@@ -7,10 +7,17 @@ namespace exercise_52
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      int yourNumber = Convert.ToInt32(Console.ReadLine());
+      PrintFromNumberToOne(yourNumber);
     }
 
     // Write your method here:
-
+    public static void PrintFromNumberToOne(int number)
+    {
+      for(int i=number;i>0;i--)
+      {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
