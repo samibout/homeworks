9a14aa8b91596523e24829b7c2be12646f57f53d
Sammien <sammi.sjodahl@passagen.se>
2020-01-29 21:04:17 +0200

    ex59done

--- part2/methods/exercise_59/Program.cs
+++ part2/methods/exercise_59/Program.cs
@@ -11,6 +11,20 @@ namespace exercise_59
     }
 
     // Write your method here:
-
+    public static int Greatest(int vl1, int vl2, int vl3)
+    {
+      if(vl1>vl2 && vl1>vl3)
+      {
+        return vl1;
+      }
+      else if(vl2>vl3)
+      {
+        return vl2;
+      }
+      else
+      {
+        return vl3;
+      }
+    }
   }
 }
