12b08322ad4156714fbbfd9121eca22035404b14
Sammien <sammi.sjodahl@passagen.se>
2020-01-29 09:51:10 +0200

    ex54done

--- part2/methods/exercise_54/Program.cs
+++ part2/methods/exercise_54/Program.cs
@@ -7,10 +7,22 @@ namespace exercise_54
     public static void Main(String[] args)
     {
       // Call your method here:
+      int first=8;
+      int last=27;
+      DivisibleByThreeInRange(first,last);
 
     }
 
     // Write your method here:
-
+    public static void DivisibleByThreeInRange(int beginning, int end)
+    {
+      for(int i=beginning;i<=end;i++)
+      {
+        if(i%3==0)
+        {
+          Console.WriteLine(i);
+        }
+      }
+    }
   }
 }
