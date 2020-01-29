4ab941cb93dbe424b45be8151e52300efc4bfe89
Sammien <sammi.sjodahl@passagen.se>
2020-01-29 09:43:07 +0200

    ex53done

--- part2/methods/exercise_53/Program.cs
+++ part2/methods/exercise_53/Program.cs
@@ -7,11 +7,18 @@ namespace exercise_53
     public static void Main(String[] args)
     {
       // Call your method here:
-
+      int num=5;
+      int den=3;
+      Division(num,den);
 
     }
 
     // Write your method here:
-
+    public static void Division(int numerator, int denominator)
+    {
+      double numerat = numerator;
+      double division = numerat/denominator;
+      Console.WriteLine(division);
+    }
   }
 }
