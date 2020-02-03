c238c805bccdbf1c8d7eb7c60a486130974b5ae5
Sammien <sammi.sjodahl@passagen.se>
2020-02-03 19:33:13 +0200

    ex72done

--- part3/lists/exercise_72/Program.cs
+++ part3/lists/exercise_72/Program.cs
@@ -7,6 +7,38 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+      List<int> numbers = new List<int>();
+
+      while(true)
+      {
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if(number==9999)
+        {
+          break;
+        }
+        numbers.Add(number);
+      }
+      //search minimum
+      int min = numbers[0];
+      foreach(int i in numbers)
+      {
+        if(i<min)
+        {
+          min=i;
+        }
+      }
+
+      Console.WriteLine("Smallest number: " + min);
+
+      //search indexes of the minimum values
+      for(int i=0;i<numbers.Count;i++)
+      {
+        if(numbers[i]==min)
+        {
+          Console.WriteLine("Found at index: " + i);
+        }
+      }
 
     }
   }
