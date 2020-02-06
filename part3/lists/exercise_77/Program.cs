e5d8ed917775242cd95c743daad4f2de9e47bff8
Sammien <sammi.sjodahl@passagen.se>
2020-02-06 16:52:28 +0200

    ex77done

--- part3/lists/exercise_77/Program.cs
+++ part3/lists/exercise_77/Program.cs
@@ -7,7 +7,25 @@ namespace exercise_77
   {
     public static void Main(string[] args)
     {
+      List<string> strings = new List<string>();
+      strings.Add("First");
+      strings.Add("Second");
+      strings.Add("Third");
+
+      RemoveLast(strings);
+
+      strings.ForEach(Console.WriteLine);
       // You can try your method here if you want
+    }
+
+    public static void RemoveLast(List<string> strings)
+    {
+       if(strings.Count == 0)
+       {
+         return;
+       }
+       strings.RemoveAt(strings.Count-1);
+      
     }
 
   }
