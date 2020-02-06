3dbc1eec57fb09cc5e4141d6f4d127ce17825640
Sammien <sammi.sjodahl@passagen.se>
2020-02-06 16:28:17 +0200

    ex76done

--- part3/lists/exercise_76/Program.cs
+++ part3/lists/exercise_76/Program.cs
@@ -7,9 +7,23 @@ namespace exercise_76
   {
     public static void Main(string[] args)
     {
+      List<int> numbers = new List<int>();
+      numbers.Add(3);
+      numbers.Add(6);
+      numbers.Add(-2);
+
+      Console.WriteLine(Sum(numbers));
 
     }
-
+    public static int Sum(List<int> numbers)
+    {
+      int sum = 0;
+      foreach(int i in numbers)
+      {
+        sum = sum + i;
+      }
+      return sum;
+    }
   }
 }
 
