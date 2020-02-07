af941fbf53dad463ae34e1fc5fb189933a7ecece
Sammien <sammi.sjodahl@passagen.se>
2020-02-07 17:32:13 +0200

    ex81done

--- part3/arrays/exercise_81/Program.cs
+++ part3/arrays/exercise_81/Program.cs
@@ -8,12 +8,22 @@ namespace exercise_81
     public static void Main(string[] args)
     {
       // You can test your method here
+      int[] array = {5, 1, 3, 4, 2};
+      PrintNeatly(array);
 
     }
 
     public static void PrintNeatly(int[] array)
     {
+      for(int i=0;i<array.Length-1;i++)
+        {
+          if(i< (array.Length-1))
+          {
+            Console.Write(array[i] + ", ");
+          }
+        }
 
+      Console.WriteLine(array[array.Length-1]);
     }
   }
 }
