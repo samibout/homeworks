12cc13ce383749f5b1feb5c075eafd03bf3c53db
Sammien <sammi.sjodahl@passagen.se>
2020-02-09 12:46:58 +0200

    ex88done

--- part3/strings/exercise_88/Program.cs
+++ part3/strings/exercise_88/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_88
   {
     public static void Main(string[] args)
     {
-
+      string input;
+      while(true)
+      {
+        input = Console.ReadLine();
+        if(input=="")
+        {
+          break;
+        }
+        string[] pieces = input.Split(" ");
+        Console.WriteLine(pieces[0]);
+      }
     }
   }
 }
