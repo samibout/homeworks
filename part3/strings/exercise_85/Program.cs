72a9e79b9623a89e6eae8020270f66dbb93ea1a0
Sammien <sammi.sjodahl@passagen.se>
2020-02-09 08:52:44 +0200

    ex85done

--- part3/strings/exercise_85/Program.cs
+++ part3/strings/exercise_85/Program.cs
@@ -7,7 +7,32 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Enter username:");
+      string username = Console.ReadLine();
 
+      Console.WriteLine("Enter password:");
+      string password = Console.ReadLine();
+
+      
+
+      CheckLogin(username,password);
+      
+    }
+
+    public static void CheckLogin(string username, string password)
+    {
+      string[] usernames = {"alex","emma"};
+      string[] passwords = {"sunshine","haskell"};
+
+      for(int i=0;i<usernames.Length;i++)
+        {
+          if(usernames[i]==username && passwords[i]==password)
+          {
+            Console.WriteLine("You have successfully logged in!");
+            return;
+          }
+        }
+        Console.WriteLine("Incorrect username or password!");
 
     }
   }
