c145721d59c73f9f250781bc4b97b003f2608445
Sammien <sammi.sjodahl@passagen.se>
2020-02-09 09:39:17 +0200

    ex87done

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -7,9 +7,31 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
+      string input = "";
+      while(true)
+      {
+        input = Console.ReadLine(); 
+        if(input=="")
+        {
+          break;
+        }
+         
+      string[] pieces = input.Split(" ");
 
+      StringContains(pieces);
+      }
     }
 
+    public static void StringContains(string[] words)
+    {
+      foreach(string word in words)
+      {
+        if(word.Contains("av"))
+        {
+          Console.WriteLine(word);
+        }
+      }
+    }
   }
 
 }
