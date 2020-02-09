bc866a3699c10c3fed4366ff1d3eaec180097336
Sammien <sammi.sjodahl@passagen.se>
2020-02-09 13:47:06 +0200

    ex90done

--- part3/strings/exercise_90/Program.cs
+++ part3/strings/exercise_90/Program.cs
@@ -7,6 +7,25 @@ namespace exercise_90
   {
     public static void Main(string[] args)
     {
+      int greatest = 0;
+
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        string[] parts = input.Split(",");
+
+        if(Convert.ToInt32(parts[1]) > greatest)
+        {
+          greatest = Convert.ToInt32(parts[1]);
+        }
+      }
+      Console.WriteLine("Age of the oldest: " + greatest);
+
+      
 
 
     }
