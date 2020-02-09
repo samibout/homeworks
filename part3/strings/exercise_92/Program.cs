c86c98652b6238ea985a1e3d36799996db602978
Sammien <sammi.sjodahl@passagen.se>
2020-02-09 14:52:17 +0200

    ex92done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,7 +7,37 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      string input;
+      string[] split;
+      string longest="";
+      int length=0;
+      int age=0;
+      int year=2020;
 
+      while(true)
+      {
+      
+       input=Console.ReadLine();
+       if(input=="")
+       {
+         break;
+       }
+       split=input.Split(",");
+
+       if(Convert.ToInt32(split[1])<year)
+       {
+         year=Convert.ToInt32(split[1]);
+       }
+       if(split[0].Length>length)
+       {
+         longest = split[0];
+         length = split[0].Length;
+       }
+
+      }
+      age = 2020 - year;
+      Console.WriteLine("Longest name: " + longest);
+      Console.WriteLine("Highest age: " + age);
     }
   }
 }
