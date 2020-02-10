6038d25cfa7f6ec2adf825c809de692609f8f44f
Sammien <sammi.sjodahl@passagen.se>
2020-02-10 18:29:23 +0200

    ex97done

---
+++ part4/objects/exercise_97/Whistle.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace exercise_97
+{
+    public class Whistle
+    {       
+        private string sound;
+        public Whistle(string whistleSound)
+        {
+        this.sound = whistleSound; 
+        }
+
+        public void Sound()
+        {
+            Console.WriteLine(sound);
+        }
+    }
+}
