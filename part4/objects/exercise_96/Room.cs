fe6a53dbb87408ff43d91a10644bf9a7b59d25f2
Sammien <sammi.sjodahl@passagen.se>
2020-02-10 17:37:00 +0200

    ex96done

---
+++ part4/objects/exercise_96/Room.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace exercise_96
+{
+    public class Room
+    {
+        private string code;
+        private int seats;
+
+        public Room(string classCode, int numberOfSeats)
+        {
+            this.code = classCode;
+            this.seats = numberOfSeats;
+        }
+    }
+}
