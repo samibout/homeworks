52ceaa1e9f1156dcdaa656ecf439e29381875ec9
Sammien <sammi.sjodahl@passagen.se>
2020-02-10 17:12:02 +0200

    ex94done

--- part4/objects/exercise_94/Program.cs
+++ part4/objects/exercise_94/Program.cs
@@ -8,7 +8,14 @@ namespace exercise_94
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account heikkisAccount = new Account("Heikki's account", 1000.0);
+      Account personalAccount = new Account("Personal account", 0);
 
+      heikkisAccount.Withdrawal(100.0);
+      personalAccount.Deposit(100.0);
+
+      Console.WriteLine(heikkisAccount);
+      Console.WriteLine(personalAccount);
     }
   }
 }
