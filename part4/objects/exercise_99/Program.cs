449fb0ada597b87f11d8c76621bd5d840966ffa0
Sammien <sammi.sjodahl@passagen.se>
2020-02-11 18:07:25 +0200

    ex99done

--- part4/objects/exercise_99/Program.cs
+++ part4/objects/exercise_99/Program.cs
@@ -9,6 +9,13 @@ namespace exercise_99
 
       DecreasingCounter counter = new DecreasingCounter(20);
       counter.PrintValue();
+      counter.Decrement();
+      counter.PrintValue();
+      counter.Decrement();
+      counter.PrintValue();
+      counter.Decrement();
+      counter.PrintValue();
+      counter.Decrement();
 
       counter.Reset();
       counter.PrintValue();
