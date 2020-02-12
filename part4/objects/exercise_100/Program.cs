8e7f583351459bf444c2b09353f9343c90560b02
Sammien <sammi.sjodahl@passagen.se>
2020-02-12 18:10:20 +0200

    ex100done

--- part4/objects/exercise_100/Program.cs
+++ part4/objects/exercise_100/Program.cs
@@ -6,7 +6,16 @@ namespace exercise_100
   {
     public static void Main(string[] args)
     {
+      Debt mortgage = new Debt(120000.0, 1.01);
+      mortgage.PrintBalance();
+      mortgage.WaitOneYear();
+      mortgage.PrintBalance();
 
+      for(int i=0;i<20;i++)
+      {
+        mortgage.WaitOneYear();
+      }
+      mortgage.PrintBalance();
     }
   }
 }
