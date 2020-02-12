f93a3e8ccc034c80118045a129a6eaf2b9342d2d
Sammien <sammi.sjodahl@passagen.se>
2020-02-12 18:25:44 +0200

    ex101done

---
+++ part4/objects/exercise_101/Dalmatian.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace exercise_101
+{
+    public class Dalmatian
+    {
+        public string name{get;set;}
+        public int spots{get;set;}
+
+        public Dalmatian(string name, int spots)
+        {
+            this.name = name;
+            this.spots = spots;
+        }
+    }
+}
