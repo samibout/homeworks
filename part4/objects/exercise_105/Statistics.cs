5751d50b123b99a6271279882eb5204538d898d8
Sammien <sammi.sjodahl@passagen.se>
2020-02-13 09:17:17 +0200

    ex105done

--- part4/objects/exercise_105/Statistics.cs
+++ part4/objects/exercise_105/Statistics.cs
@@ -8,12 +8,15 @@ namespace exercise_105
     public Statistics()
     {
       // initialize the variables here
+      this.count = 0;
+      this.sum = 0;
     }
 
     public void AddNumber(int number)
     {
       // write code here
-      
+      count++;
+      this.sum = this.sum + number;
     }
   }
 }
