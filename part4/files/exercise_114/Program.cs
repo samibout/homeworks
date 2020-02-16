7eabec06b9ca83f194a730b834de4b0166cdfe33
Sammien <sammi.sjodahl@passagen.se>
2020-02-16 14:31:44 +0200

    ex114done

--- part4/files/exercise_114/Program.cs
+++ part4/files/exercise_114/Program.cs
@@ -6,6 +6,18 @@ namespace exercise_114
     {
         public static void Main(string[] args)
         {
+            int counter=0;
+            while(true)
+
+            {
+                string input = Console.ReadLine();
+                if(input=="end")
+                {
+                    break;
+                }
+                counter++;
+            }
+            Console.WriteLine(counter);
 
         }
     }
