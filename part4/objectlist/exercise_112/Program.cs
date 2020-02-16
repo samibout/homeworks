9547ab9f22f16252a6b6fe2a14a7d9880e9f950a
Sammien <sammi.sjodahl@passagen.se>
2020-02-16 09:56:14 +0200

    ex112done

--- part4/objectlist/exercise_112/Program.cs
+++ part4/objectlist/exercise_112/Program.cs
@@ -8,8 +8,29 @@ namespace exercise_112
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+      List<TelevisionProgram> list = new List<TelevisionProgram>();
+      while(true)
+      {
+        Console.Write("Name: ");
+        string prName = Console.ReadLine();
+        if(prName=="")
+        {
+          break;
+        }
+        Console.Write("Duration: ");
+        int prDuration = Convert.ToInt16(Console.ReadLine());
+        list.Add(new TelevisionProgram(prName, prDuration));
+      }
+      Console.Write("\nProgram's maximum duration? ");
+      int maxD = Convert.ToInt16(Console.ReadLine());
 
-
+      foreach(TelevisionProgram blaa in list)
+      {
+        if(blaa.duration <= maxD)
+        {
+          Console.WriteLine(blaa);
+        }
+      }
     }
   }
 }
