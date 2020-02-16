13b97679b871084ac91e4194cd305f4bd059e927
Sammien <sammi.sjodahl@passagen.se>
2020-02-16 14:40:49 +0200

    ex115done

--- part4/files/exercise_115/Program.cs
+++ part4/files/exercise_115/Program.cs
@@ -6,7 +6,18 @@ namespace exercise_115
   {
     public static void Main(string[] args)
     {
-
+      string input;
+      while(true)
+      {
+        input = Console.ReadLine();
+        if(input=="end")
+        {
+          break;
+        }
+      
+        int toCube = Convert.ToInt32(input);
+        Console.WriteLine(toCube*toCube* toCube);
+      }
     }
   }
 }
