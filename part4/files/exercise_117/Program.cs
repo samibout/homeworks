e110d4a18b72a3bfcc44bf16518a3c7ed7702ae8
Sammien <sammi.sjodahl@passagen.se>
2020-02-16 15:07:19 +0200

    ex117done

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -8,7 +8,11 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
+      Console.WriteLine("Which file should have its contents printed?");
+      string content = Console.ReadLine() ;
 
+      string text = File.ReadAllText(content);
+      Console.WriteLine(text);
       
     }
   }
