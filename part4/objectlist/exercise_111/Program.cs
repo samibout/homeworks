f4a1f011f6993e123b9bcd5a1f743ca3c705f683
Sammien <sammi.sjodahl@passagen.se>
2020-02-16 09:25:58 +0200

    ex111done

--- part4/objectlist/exercise_111/Program.cs
+++ part4/objectlist/exercise_111/Program.cs
@@ -9,7 +9,30 @@ namespace exercise_111
     {
       // IMPLEMENT YOUR CODE IN HERE!
       List<PersonalInformation> list = new List<PersonalInformation>();
-
+      List<string> aList = new List<string>();
+      while(true)
+      {
+        Console.WriteLine("First name:");
+        string fName = Console.ReadLine();
+        if(fName=="")
+        {
+          break;
+        }
+        Console.WriteLine("Last name:");
+        string lName = Console.ReadLine();
+        Console.WriteLine("Identification number:");
+        string id = Console.ReadLine();
+        list.Add(new PersonalInformation(fName, lName, id));
+        string fullName = fName + " " + lName;
+        aList.Add(fullName);
+        
+      }
+        Console.WriteLine();
+        foreach(string lame in aList)
+        {
+          Console.WriteLine(lame);
+        }
+      
     }
   }
 }
