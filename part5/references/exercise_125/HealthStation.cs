9ad0192e37307d239e8ebb1672b381092e91c008
Sammien <sammi.sjodahl@passagen.se>
2020-02-23 09:06:31 +0200

    ex125done

--- part5/references/exercise_125/HealthStation.cs
+++ part5/references/exercise_125/HealthStation.cs
@@ -10,12 +10,14 @@ namespace exercise_125
     {
       // return the weight of the person passed as the parameter
       // incrase weighings
-      return 0;
+      weighings = weighings + 1;
+      return person.weight;
     }
 
     public void Feed(Person person)
     {
       // Increse the weight of the person with one kilogram
+      person.weight = person.weight + 1;
     }
   }
 }
