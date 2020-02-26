68cc29ed82dfb911dfbf6058480787afda852945
Sammien <sammi.sjodahl@passagen.se>
2020-02-26 10:08:38 +0200

    ex129done

--- part5/references/exercise_129/Program.cs
+++ part5/references/exercise_129/Program.cs
@@ -7,7 +7,17 @@ namespace exercise_129
     public static void Main(string[] args)
     {
       // Try your code here, if you want
-      
+      Song same = new Song("abba", "boo", 235);
+      Song sameSame = new Song("abba", "bo", 235);
+
+      if(same.Equals(sameSame))
+      {
+        Console.WriteLine("Songs are equal.");
+      }
+      if(same.Equals("Another object"))
+      {
+        Console.WriteLine("Strange things are afoot.");
+      }
     }
 
   }
--- part5/references/exercise_129/Song.cs
+++ part5/references/exercise_129/Song.cs
@@ -1,3 +1,4 @@
+
 namespace exercise_129
 {
 
@@ -23,10 +24,26 @@ namespace exercise_129
     // BEGIN SOLUTION
     public override bool Equals(object compared)
     {
- 
-      return false;
-    }
+      if (this == compared)
+      {
+        return true;
+      }
+    
+      if((compared == null) || !this.GetType().Equals(compared.GetType()) )
+      {
+        return false;
+      }
+      Song comparedSong = (Song)compared;
+      if(this.artist == comparedSong.artist &&
+          this.name == comparedSong.name &&
+          this.durationInSeconds == comparedSong.durationInSeconds)
+      {
+        return true;
+      } 
+
+      return false;   
     // END SOLUTION
+    }
 
   }
 
