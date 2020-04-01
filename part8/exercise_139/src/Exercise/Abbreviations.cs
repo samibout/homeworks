1d3d7aedb312adedcc9128f6725f947c8df0e7df
Sammien <sammi.sjodahl@passagen.se>
2020-04-01 17:51:47 +0300

    ex138done

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -4,20 +4,45 @@ namespace Exercise
 {
   public class Abbreviations
   {
+    private Dictionary<string, string> dicAbbreviation;
+
+    public Abbreviations()
+    {
+      this.dicAbbreviation = new Dictionary<string, string>();
+    }
 
     public void AddAbbreviation(string abbreviation, string explanation)
     {
-
+        if(HasAbbreviation(abbreviation)==false)
+        
+        {
+          dicAbbreviation.Add(abbreviation, explanation);
+        }
     }
 
     public bool HasAbbreviation(string abbreviation)
     {
+      foreach(string abb in dicAbbreviation.Keys)
+      {
+        if(abb == abbreviation)
+        {
+          return true;
+        }
+      }  
+  
       return false;
+          
+        
     }
 
     public string FindExplanationFor(string abbreviation)
     {
-      return null;
+      if(HasAbbreviation(abbreviation)==true)
+      {
+        return this.dicAbbreviation[abbreviation];
+      }
+      else
+      return "not found";
     }
   }
 }
