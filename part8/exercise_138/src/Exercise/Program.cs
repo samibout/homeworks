620388420695c58b75bd7d5778cb9d519a95244a
Sammien <sammi.sjodahl@passagen.se>
2020-04-01 10:39:09 +0300

    ex138done

--- part8/exercise_138/src/Exercise/Program.cs
+++ part8/exercise_138/src/Exercise/Program.cs
@@ -8,8 +8,17 @@ namespace Exercise
     public static void Main(string[] args)
     {
       // Do something here
+      Dictionary<string, string> names = new Dictionary<string, string>();
 
+      names.Add("matthew", "matt");
+      names.Add("michael", "mix");
+      names.Add("arthur", "artie");
+
+      foreach(string nameAndNick in names.Keys)
+      {
+        Console.WriteLine(nameAndNick + "'s nickname is " + names[nameAndNick]);
       }
+      
     }
   }
 }
