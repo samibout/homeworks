822a71e998ef93fd58b853f3b2a52ee9e873ab9e
Sammien <sammi.sjodahl@passagen.se>
2020-04-28 17:40:57 +0300

    ex149done

--- part9/exercise_149/src/Exercise/People/Person.cs
+++ part9/exercise_149/src/Exercise/People/Person.cs
@@ -2,5 +2,17 @@ namespace Exercise
 {
   public class Person
   {
+    public string name;
+    public string address;
+
+    public Person(string name, string address)
+    {
+      this.name = name;
+      this.address = address;
+    }
+    public override string ToString()
+    {
+      return name + ", " + address;
+    }
   }
 }
--- part9/exercise_149/src/Exercise/People/Student.cs
+++ part9/exercise_149/src/Exercise/People/Student.cs
@@ -1,6 +1,25 @@
 namespace Exercise
 {
-  public class Student
+  public class Student:Person
   {
+    private int credit;
+
+    public Student(string name, string address, int credit) : base(name, address)
+    {
+      this.credit = credit;
+    }
+    public Student(string name, string address) : base(name, address)
+    {
+      this.credit = 0;
+    }
+
+    public void Study()
+    {
+      this.credit++;
+    }
+    public override string ToString()
+    {
+      return base.ToString() + " " + "credits: " + credit;
+    }
   }
 }
