21e0aea001db63faa4bfe44926c80a27a9c5dcb0
Sammien <sammi.sjodahl@passagen.se>
2020-04-30 16:52:50 +0300

    ex150done

--- part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ChangeHistory.cs
@@ -8,32 +8,51 @@ namespace Exercise
 
     public ChangeHistory()
     {
-
+      this.history = new List<int>();
     }
 
     public void Add(int status)
     {
-
+      this.history.Add(status);
     }
 
     public void Clear()
     {
-
+      this.history.Clear();
     }
 
     public int MaxValue()
     {
-      return 0;
+      int mx = this.history[0];
+      foreach(int val in this.history)
+      {
+        if(val>mx)
+        {
+          mx = val;
+        }
+      }
+
+        return mx;
     }
 
     public int MinValue()
     {
-      return 0;
+      int mn = this.history[0];
+      foreach(int val in this.history)
+      {
+        if(val<mn)
+        {
+          mn = val;
+        }
+      }
+
+        return mn;
     }
 
     public override string ToString()
     {
-      return "";
+      return "Current: " + (history[history.Count-1])+ " Min: " + this.MinValue()
+      + " Max: " + this.MaxValue();
     }
   }
 }
--- part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
+++ part9/exercise_150/src/Exercise/Warehouses/ProductWareHouseWithHistory.cs
@@ -2,24 +2,31 @@ namespace Exercise
 {
   public class ProductWarehouseWithHistory : ProductWarehouse
   {
+    ChangeHistory history;
     public ProductWarehouseWithHistory(string productName, int capacity, int initialBalance) : base(productName, capacity)
     {
+      history = new ChangeHistory();
+      this.balance = initialBalance;
+      history.Add(initialBalance);
 
     }
 
     public string History()
     {
-      return "";
+      return history.ToString();
     }
 
     new public void AddToWarehouse(int amount)
     {
-
+      base.AddToWarehouse(amount);
+      this.history.Add(this.balance);
     }
 
     new public int TakeFromWarehouse(int amount)
     {
-      return 0;
+      base.TakeFromWarehouse(amount);
+      this.history.Add(this.balance);
+      return amount;
     }
   }
 }
