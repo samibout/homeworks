6dbba68a1778f1051db2f91f79f6838cf229f5bf
Sammien <sammi.sjodahl@passagen.se>
2020-05-06 09:53:01 +0300

    ex152done

--- part9/exercise_152/src/Exercise/Tacos/CustomTacoBox.cs
+++ part9/exercise_152/src/Exercise/Tacos/CustomTacoBox.cs
@@ -2,18 +2,23 @@ namespace Exercise
 {
   public class CustomTacoBox : ITacoBox
   {
+    private int tacoAmount;
     public CustomTacoBox(int tacos)
     {
+      this.tacoAmount = tacos;
     }
 
     public int TacosRemaining()
     {
-      return 0;
+      return this.tacoAmount;
     }
 
     public void Eat()
     {
-
+      if(tacoAmount>0)
+      {
+        tacoAmount--;
+      }
     }
   }
 }
--- part9/exercise_152/src/Exercise/Tacos/TripleTacoBox.cs
+++ part9/exercise_152/src/Exercise/Tacos/TripleTacoBox.cs
@@ -2,14 +2,23 @@ namespace Exercise
 {
   public class TripleTacoBox : ITacoBox
   {
+    private int tacoAmount;
+    public TripleTacoBox()
+    {
+      this.tacoAmount = 3;
+    }
 
     public int TacosRemaining()
     {
-      return 0;
+      return this.tacoAmount;
     }
 
     public void Eat()
     {
+      if(tacoAmount>0)
+      {
+        this.tacoAmount--;
+      }
 
     }
   }
